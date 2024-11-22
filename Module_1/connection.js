2d7f2d99cdbb5b45e6af6b12846ad1ad454906f0
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-11-21 23:07:56 -0500

    Refactor MongoDB connection setup for improved readability and efficiency

--- Module_1/connection.js
+++ Module_1/connection.js
@@ -1,33 +1,19 @@
-require('dotenv').config();
-// Import the MongoDB client
+require('dotenv').config();  // Ensure this line is at the very top of your script
 const { MongoClient } = require('mongodb');
-
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const name = process.env.DB_NAME;
-// Replace the URL with your actual MongoDB connection string
-const url = 'mongodb+srv://${username}:${password}@db1.8qhzp.mongodb.net/'; // Or your MongoDB Atlas connection string
-const dbName = name;
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@db1.8qhzp.mongodb.net/`;
+const dbName = process.env.DB_NAME;
 
 async function connectToMongoDB() {
-  // Create a new MongoClient
-  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(url);
 
   try {
-    // Connect the client to the server
     await client.connect();
     console.log('Connected successfully to MongoDB server');
-
-    // Select the database you want to work with
     const db = client.db(dbName);
-
-    // Perform database operations here...
-
     return db;
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
   } finally {
-    // Close the client when done
     await client.close();
   }
 }
